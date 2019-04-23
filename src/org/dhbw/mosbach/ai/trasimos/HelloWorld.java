908119e232e835c1990fb1165b7de170cebda9be
MSINF16B <mic.spahr.16@lehre.mosbach.dhbw.de>
2019-04-23 08:12:59 +0200

    Task 1.5

---
+++ src/org/dhbw/mosbach/ai/trasimos/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.dhbw.mosbach.ai.trasimos;
+
+/**
+ * 
+ * @author 3040018
+ */
+public class HelloWorld {
+
+	public HelloWorld() {
+		System.out.println("Constructor HelloWorld");
+	}
+	
+	public void startup() {
+		System.out.println("startup() HelloWorld");
+	}
+}
