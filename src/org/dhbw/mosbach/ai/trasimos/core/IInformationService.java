57c91a64f6fecb36a596f87cd464c12f07407618
Morten Terhart <morten.terhart@gmail.com>
2019-05-13 09:09:03 +0200

    Removed duplicate domain name package structure under already existing structure

--- src/org/dhbw/mosbach/ai/trasimos/core/IInformationService.java
+++ src/org/dhbw/mosbach/ai/trasimos/core/IInformationService.java
@@ -1,10 +1,14 @@
-package org.dhbw.mosbach.ai.trasimos.core.org.dhbw.mosbach.ai.trasimos.remoteInterfaces;
+package org.dhbw.mosbach.ai.trasimos.core;
 
 import org.dhbw.mosbach.ai.trasimos.core.model.Position;
 
 public interface IInformationService {
+
     public void receivePosition(long ID, Position position);
+
     public void getNeighbours(long ID, double speed);
+
     public void overtakeInformationService(Position position00, Position position10, Position position01, Position position11);
+
     public void receiveFinished(long ID);
 }
--- src/org/dhbw/mosbach/ai/trasimos/core/IWebserver.java
+++ src/org/dhbw/mosbach/ai/trasimos/core/IWebserver.java
@@ -1,7 +1,8 @@
-package org.dhbw.mosbach.ai.trasimos.core.org.dhbw.mosbach.ai.trasimos.remoteInterfaces;
+package org.dhbw.mosbach.ai.trasimos.core;
 
 import org.dhbw.mosbach.ai.trasimos.core.model.Position;
 
 public interface IWebserver {
+
     public void receivePosition(long ID, Position position);
 }
