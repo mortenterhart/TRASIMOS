f29c44b443470534532db72e5dc8b64399e299fe
hansfuchs <f.hanschristian@gmail.com>
2019-05-20 11:19:26 +0200

    merged old branch with new version of master

--- bundles/org.dhbw.mosbach.ai.v2.provider/src/org/dhbw/mosbach/ai/v2/provider/V2Impl.java
+++ bundles/org.dhbw.mosbach.ai.v2.provider/src/org/dhbw/mosbach/ai/v2/provider/V2Impl.java
@@ -3,17 +3,29 @@ package org.dhbw.mosbach.ai.v2.provider;
 import java.util.Map;
 import java.util.Vector;
 
-import org.dhbw.mosbach.ai.v2.api.IV2;
-import org.dhbw.mosbach.ai.v2.api.model.Position;
+
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 
+import javax.swing.text.Position;
+
 @Component(name = "v2", service = IV2.class)
 public class V2Impl implements IV2 {
-    
+    private int id;
+    private Position origin;
+    private Position destination;
+    private Position currentPosition;
+    private Vector<Long> direction;
+    private double velocity;
+
+    public V2Impl(double originLongitude, double originLatitude, double destinationLongitude, double destinationLatitude) {
+        origin = new Position(originLongitude, originLatitude);
+        destination = new Position(destinationLongitude, destinationLatitude);
+    }
+
     @Activate
     public void activate(ComponentContext context, BundleContext bundleContext, Map<String, ?> properties) {
         System.out.println("V2 booting ...");
@@ -24,8 +36,35 @@ public class V2Impl implements IV2 {
         System.out.println("V2 shutting down ...");
     }
 
+    public Position getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public Position getDestination() {
+        return destination;
+    }
+
+    public Position getOrigin() {
+        return origin;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void run() {
+        boolean isDriving = true;
+        while (isDriving) {
+            try {
+                Thread.sleep(TIMEOUT);
+                System.out.println("Driving");
+            } catch (InterruptedException exc) {
+                exc.printStackTrace();
+            }
+        }
+    }
+
     @Override
-    public void getPosition(Position position, Vector<Long> direction, double speed) {
+    public Position getCurrentPosition() {
 
     }
 
