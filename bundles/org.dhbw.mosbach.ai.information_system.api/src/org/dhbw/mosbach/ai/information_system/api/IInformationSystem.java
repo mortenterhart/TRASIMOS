68f8bcbb3bed00e17a02e6c41ff55df3ba9e3b22
JacobNr16 <jac.krauth.16@lehre.mosbach.dhbw.de>
2019-05-20 16:08:32 +0200

    Logic of information system implemented

--- bundles/org.dhbw.mosbach.ai.information_system.api/src/org/dhbw/mosbach/ai/information_system/api/IInformationSystem.java
+++ bundles/org.dhbw.mosbach.ai.information_system.api/src/org/dhbw/mosbach/ai/information_system/api/IInformationSystem.java
@@ -2,11 +2,15 @@ package org.dhbw.mosbach.ai.information_system.api;
 
 import org.dhbw.mosbach.ai.information_system.api.model.Position;
 
+import java.util.List;
+
 public interface IInformationSystem {
 
     public void receivePosition(long v2Id, Position position);
 
-    public void getNeighbours(long v2Id, double speed);
+    public List<Position> getNeighbours(long v2Id, double speed);
+
+    public List<Position> getNeighbours(Position position, double radius);
 
     public void overtakeInformationService(Position position00, Position position10, Position position01, Position position11);
 
--- bundles/org.dhbw.mosbach.ai.information_system.provider/src/org/dhbw/mosbach/ai/information_system/provider/InformationSystemImpl.java
+++ bundles/org.dhbw.mosbach.ai.information_system.provider/src/org/dhbw/mosbach/ai/information_system/provider/InformationSystemImpl.java
@@ -1,6 +1,10 @@
 package org.dhbw.mosbach.ai.information_system.provider;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.lang.Math;
 
 import org.dhbw.mosbach.ai.information_system.api.IInformationSystem;
 import org.dhbw.mosbach.ai.information_system.api.IPublishPosition;
@@ -13,6 +17,7 @@ import org.osgi.service.component.ComponentContext;
 
 @Component(name = "information-system", service = {IInformationSystem.class, IPublishPosition.class})
 public class InformationSystemImpl implements IInformationSystem, IPublishPosition {
+    private HashMap<Long, Position> vehiclesToObserve = new HashMap<Long, Position>();
 
     @Activate
     public void activate(ComponentContext context, BundleContext bundleContext, Map<String, ?> properties) {
@@ -26,27 +31,85 @@ public class InformationSystemImpl implements IInformationSystem, IPublishPositi
 
     @Override
     public void receivePosition(long v2Id, Position position) {
-
+        // Add/Update vehicle in Map
+        // if vehicle id already exists the position of vehicle will be overridden
+        vehiclesToObserve.put(v2Id, position);
     }
 
+    /**
+     * get neighbours of vehicle by its id and speed.
+     * requres that vehicle is observed by this information system
+     * @param v2Id id of v2 vehicle
+     * @param speed speed of v2 vehicle
+     * @return list of positions of neighbours
+     */
     @Override
-    public void getNeighbours(long v2Id, double speed) {
+    public List<Position> getNeighbours(long v2Id, double speed) {
+        // If vehicle is new, neighbours can not be resolved
+        // Vehicle needs to publish its position first
+        if (isVehicleKnown(v2Id)) {
+            List<Position> positionOfNeighbours = new ArrayList<>();
+            // stopping distance = reaction time distance + actual stopping distance
+            double stoppingDistance = (speed / 10 * 3) + (speed / 10 * speed / 10);
+            Position positionOfVehicle = vehiclesToObserve.get(v2Id);
 
+            for (Position pos : vehiclesToObserve.values()) {
+                if (distanceBetweenPositions(positionOfVehicle, pos) <= stoppingDistance) {
+                    positionOfNeighbours.add(pos);
+                }
+            }
+
+            return positionOfNeighbours;
+        }
+        return null;
+    }
+
+    /**
+     * get neighbours of position in a specific radius
+     * @param position centre position
+     * @param radius radius around position
+     * @return list of positions of neighbours
+     */
+    @Override
+    public List<Position> getNeighbours(Position position, double radius){
+        List<Position> positionOfNeighbours = new ArrayList<>();
+
+        for (Position pos : vehiclesToObserve.values()) {
+            if (distanceBetweenPositions(position, pos) <= radius) {
+                positionOfNeighbours.add(pos);
+            }
+        }
+
+        return positionOfNeighbours;
     }
 
     @Override
     public void overtakeInformationService(Position position00, Position position10, Position position01,
-            Position position11) {
+                                           Position position11) {
 
     }
 
+    /**
+     * remove vehicle from observing
+     * @param v2Id id of v2 vehicle
+     */
     @Override
     public void receiveFinished(long v2Id) {
-
+        vehiclesToObserve.remove(v2Id);
     }
-    
+
     @Override
     public void publish(Position position) {
-        
+
+    }
+
+    private double distanceBetweenPositions(Position a, Position b) {
+        return Math.sqrt(
+                (b.getLatitude() - a.getLatitude()) * (b.getLatitude() - a.getLatitude()) +
+                        (b.getLongitude() - a.getLongitude()) * (b.getLongitude() - a.getLongitude()));
+    }
+
+    private boolean isVehicleKnown(long vehicleId) {
+        return vehiclesToObserve.containsKey(vehicleId);
     }
 }
