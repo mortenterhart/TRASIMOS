03b3857a5603dcf88218dbbfbba3089cf79c1b6d
captainblubb <fabianschulz@hotmail.de>
2019-05-24 13:53:39 +0200

    remove unusedClass

--- bundles/org.dhbw.mosbach.ai.v2.api/src/org/dhbw/mosbach/ai/v2/api/IV2.java
+++ bundles/org.dhbw.mosbach.ai.v2.api/src/org/dhbw/mosbach/ai/v2/api/IV2.java
@@ -2,12 +2,11 @@ package org.dhbw.mosbach.ai.v2.api;
 
 import org.dhbw.mosbach.ai.base.Position;
 
-import java.util.Vector;
-
 public interface IV2 {
 
+   V2Info getV2Information();
    Position getCurrentPosition();
    Position getDestination();
    Position getOrigin();
-   int getId();
+   long getId();
 }
